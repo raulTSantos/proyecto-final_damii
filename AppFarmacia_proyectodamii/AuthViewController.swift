b2f0b82d3002248d92d3f2dd9ea7819c8f40fbc6
RaulMacOS <raulmacos@RaulMacOSs-iMac.local>
2021-11-30 15:35:08 -0500

    proyecto

---
+++ AppFarmacia_proyectodamii/AuthViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AuthViewController.swift
+//  AppFarmacia_proyectodamii
+//
+//  Created by RaulMacOS on 11/30/21.
+//  Copyright © 2021 RaulMacOS. All rights reserved.
+//
+
+import UIKit
+
+class AuthViewController: UIViewController {
+
+    @IBOutlet weak var txtEmail: UITextField!
+    
+    @IBOutlet weak var txtClave: UITextField!
+    
+    @IBOutlet weak var btnAcceder: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func btnAccederAction(_ sender: Any) {
+    }
+    
+    
+    @IBAction func btnRegistrarAction(_ sender: Any) {
+    }
+    
+    
+}
+
